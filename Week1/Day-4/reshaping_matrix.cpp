b0168bb2feaecfbb301752fdee1bd432679caa4d
AbhishekKumar <abhishek2611jj@gmail.com>
2026-06-04 11:55:21 +0530

    Day4

---
+++ Week1/Day-4/reshaping_matrix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m = mat.size();
+        int n = mat[0].size();
+        if(m * n != r * c)return mat;
+        vector<vector<int>> ans(r, vector<int>(c));
+        int k = 0;
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                ans[i][j] = mat[k / n][k % n];
+                k++;
+            }
+        }
+        return ans;
+    }
+};
